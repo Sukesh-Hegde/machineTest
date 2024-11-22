8bead66305698abeae92b898aab45168bc9e4d7f
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-11-22 22:38:48 +0530

    updated

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
-import Child from "./child";
 
 export default function App() {
-  const [messageFromChild, setMessageFromChild] = useState('')
 
-  const handleChildData = (childData) => {
-    setMessageFromChild(childData);
-  };
+
+
   return (
     <div className="App">
-      <h1>parent component</h1>
-      <p>Message from child: {messageFromChild}</p>
-      <Child sendDatatoParent={handleChildData} />
+      <h1>Welcome</h1>
     </div>
   );
 }
